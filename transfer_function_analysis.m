beb55785be1d067c7fd7ce83584a9d5170a1ca72
Bryan Keating <ru.bryankeating@gmail.com>
2025-03-12 18:12:25 -0400

    Satelite example

--- transfer_function_analysis.m
+++ transfer_function_analysis.m
@@ -31,3 +31,21 @@ disp('Zeros: ');
 disp(z2);
 disp('Gain: ');
 disp(k2);
+
+% Satelite example 3
+sysG3 = 0.0002/s^2;
+t=0:0.01:10;            % set up time vector with dt = 0.01 sec
+
+u1=[zeros(1,500),...
+    25*ones(1,10),...   % pulse of 25N, at 5 sec, for 0.1 sec duration
+    zeros(1, length(t) - 500 - 10)];      % pulse input
+
+[y1]=lsim(sysG3,u1,t);   % linear simulation
+ff=180/pi;              % conversion factor from radians to degrees
+y1=ff*y1;               % output in degree
+plot(t,u1);             % plot input signal
+plot(t,y1);             % plot output response
+
+
+
+
