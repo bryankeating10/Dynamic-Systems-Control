8078932088750d5bc0fa76c0b920d8a44787ff7b
Bryan Keating <ru.bryankeating@gmail.com>
2025-05-01 11:22:16 -0400

    Experimentation with time contant and rise/settling time

--- first_root_locus.m
+++ first_root_locus.m
@@ -2,12 +2,10 @@ clc
 clear
 close all
 
-% Define transfer function (without gain K yet)
 s = tf('s');
-G = 1 / (s*(s+2));
+G = 1 / (s * (s + 2));
 
-% Plot root locus
-figure
-rlocus(G)
-title('Root Locus of G(s) = 1 / (s(s+2))')
+% Create root locus plot using rlocusplot
+h = rlocusplot(G);
+title('Root Locus with Customized Grid')
 grid on
