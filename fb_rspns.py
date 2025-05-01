ce9e5519e25a190b90f94fb58b78bfacb40d1950
Bryan Keating <ru.bryankeating@gmail.com>
2025-05-01 09:01:59 -0400

    Renamed files

--- fb_rspns.py
+++ fb_rspns.py
@@ -25,4 +25,13 @@ plt.title('Closed-Loop Step Response with Output Gain')
 plt.xlabel('Time (s)')
 plt.ylabel('Output')
 plt.grid(True)
+
+# Plot Bode plot
+plt.figure(figsize=(10, 8))
+plt.subplot(2, 1, 1)
+plt.title('Bode Plot of Closed-Loop System with Output Gain')
+ctrl.bode(T_final, dB=True, Hz=False, plot=True)
+plt.subplot(2, 1, 2)
+plt.title('Bode Plot of Closed-Loop System with Output Gain (Phase)')
+ctrl.bode(T_final, dB=False, Hz=False, plot=True)
 plt.show()
